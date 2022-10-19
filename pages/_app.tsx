8596f86c907e53316e585819e26df89c122ed21d
ju <julienbmartel@gmail.com>
2022-10-19 16:59:58 -0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  off: {
+    white: '#b8b8b8',
+    black: '#292929',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ChakraProvider {...{theme}}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
