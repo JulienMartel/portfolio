b7ba71ebf520a396a967f5f12aac0eb4afeaf1f8
ju <julienbmartel@gmail.com>
2022-11-12 20:37:10 -0600

    single-page start

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -8,12 +8,9 @@ import {
   Box,
   Stack,
   Badge,
-  StatGroup,
   Stat,
   StatLabel,
   StatNumber,
-  StatHelpText,
-  StatArrow,
   Divider,
 } from "@chakra-ui/react";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
@@ -22,6 +19,7 @@ import {
   MdLocationPin,
   MdAccessTimeFilled,
   MdLanguage,
+  MdSchool,
 } from "react-icons/md";
 import { useSpring, animated } from "@react-spring/web";
 import { IconType } from "react-icons/lib";
@@ -100,43 +98,63 @@ export const AboutMe = () => {
           ))}
         </HStack>
 
-        <Divider opacity={0.15} />
+        <Flex justify="space-between">
+          <Stack h="min" textAlign="left" spacing={8}>
+            <Stat>
+              <StatLabel>
+                languages{" "}
+                <Icon color="#fad121" opacity={0.8} mb={-0.5} as={MdLanguage} />
+              </StatLabel>
+              <StatNumber fontSize={"lg"}>English, French</StatNumber>
+            </Stat>
 
-        <StatGroup mt={7} textAlign="left">
-          <Stat>
-            <StatLabel>
-              location{" "}
-              <Icon
-                mb={-0.5}
-                color="#fad121"
-                as={MdLocationPin}
-                opacity={0.8}
-              />
-            </StatLabel>
-            <StatNumber fontSize={"lg"}>Winnipeg, CA</StatNumber>
-          </Stat>
+            <Stat>
+              <StatLabel>
+                location{" "}
+                <Icon
+                  mb={-0.5}
+                  color="#fad121"
+                  as={MdLocationPin}
+                  opacity={0.8}
+                />
+              </StatLabel>
+              <StatNumber fontSize={"lg"}>Winnipeg, CA</StatNumber>
+            </Stat>
 
-          <Stat>
-            <StatLabel>
-              time{" "}
-              <Icon
-                color="#fad121"
-                mb={-0.5}
-                as={MdAccessTimeFilled}
-                opacity={0.8}
-              />
-            </StatLabel>
-            <StatNumber fontSize={"lg"}>{time}</StatNumber>
-          </Stat>
+            <Stat>
+              <StatLabel>
+                time{" "}
+                <Icon
+                  color="#fad121"
+                  mb={-0.5}
+                  as={MdAccessTimeFilled}
+                  opacity={0.8}
+                />
+              </StatLabel>
+              <StatNumber fontSize={"lg"}>{time}</StatNumber>
+            </Stat>
 
-          <Stat>
-            <StatLabel>
-              languages{" "}
-              <Icon color="#fad121" opacity={0.8} mb={-0.5} as={MdLanguage} />
-            </StatLabel>
-            <StatNumber fontSize={"lg"}>English, French</StatNumber>
-          </Stat>
-        </StatGroup>
+            <Stat>
+              <StatLabel>
+                education{" "}
+                <Icon color="#fad121" mb={-0.5} as={MdSchool} opacity={0.8} />
+              </StatLabel>
+              <StatNumber fontSize={"lg"}>College</StatNumber>
+            </Stat>
+          </Stack>
+
+          {/* <Divider orientation="vertical" height="400px" mx="4" /> */}
+
+          <Flex
+            justify="center"
+            align="center"
+            bg="pink"
+            sx={{ aspectRatio: "1/1" }}
+            w="70%"
+          >
+            test
+          </Flex>
+        </Flex>
 
         {/* ju#8888 */}
 
