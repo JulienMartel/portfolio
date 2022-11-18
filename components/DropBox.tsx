b9f99ea2ccd28b1a9c124c777af1ed400fb9e24f
ju <julienbmartel@gmail.com>
2022-11-17 20:11:22 -0600

    deploy

--- components/DropBox.tsx
+++ components/DropBox.tsx
@@ -9,56 +9,61 @@ interface Props {
   hovering: boolean;
   dragIndex: number;
   children: React.ReactNode;
+  ref: React.RefObject<HTMLDivElement>;
 }
 
-export const DropBox = forwardRef<HTMLDivElement, Props>(
-  ({ lastHovered, hovering, dragIndex, children }, ref) => {
-    const dropBoxProps = useSpring({
-      width: hovering ? "100%" : "0",
-      config: { bounce: 0 },
-    });
+export const DropBox = ({
+  lastHovered,
+  hovering,
+  dragIndex,
+  children,
+  ref,
+}: Props) => {
+  const dropBoxProps = useSpring({
+    width: hovering ? "100%" : "0",
+    config: { bounce: 0 },
+  });
 
-    const opacityProps = useSpring({
-      from: { opacity: 0 },
-      to: { opacity: 1 },
-      delay: 100,
-    });
+  const opacityProps = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 100,
+  });
 
-    return (
+  return (
+    <animated.div
+      ref={ref}
+      style={{
+        width: DROPBOX_SIZE + "px",
+        height: DROPBOX_SIZE + "px",
+        border: "3px solid",
+        borderColor: "var(--chakra-colors-off-white)",
+        borderRadius: "12px",
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 10,
+        ...opacityProps,
+      }}
+    >
       <animated.div
-        ref={ref}
         style={{
-          width: DROPBOX_SIZE + "px",
-          height: DROPBOX_SIZE + "px",
-          border: "3px solid",
-          borderColor: "var(--chakra-colors-off-white)",
-          borderRadius: "12px",
-          position: "relative",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          zIndex: 10,
-          ...opacityProps,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          borderRadius: "7px",
+          zIndex: "-1",
+
+          background: hovering
+            ? content[dragIndex].color
+            : content[lastHovered].color,
+          height: "100%",
+          ...dropBoxProps,
         }}
-      >
-        <animated.div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            borderRadius: "7px",
-            zIndex: "-1",
+      />
 
-            background: hovering
-              ? content[dragIndex].color
-              : content[lastHovered].color,
-            height: "100%",
-            ...dropBoxProps,
-          }}
-        />
-
-        {children}
-      </animated.div>
-    );
-  }
-);
+      {children}
+    </animated.div>
+  );
+};
